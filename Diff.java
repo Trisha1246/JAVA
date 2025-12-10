702e5d1d501ef4d1e68c3f82d94e068670329288
Trisha1246 <kolla.Trisha2006@gmail.com>
2025-12-10 21:52:09 +0530

    Add files via upload

---
+++ Diff.java
@@ -0,0 +1,8 @@
+ public class Diff {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 20;
+        int result = num1 - num2;
+        System.out.println("The difference of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
